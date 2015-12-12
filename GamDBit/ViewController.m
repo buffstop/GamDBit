4b043d878ae99937f87551c6a5b31a1255ce1750
Andreas Buff <andreas.buff@cgm.com>
2015-12-12 02:15:03 +0100

    Initial Commit

---
+++ GamDBit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GamDBit
+//
+//  Created by Andreas Buff on 12/12/15.
+//  Copyright © 2015 Andreas Buff. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GamDBit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GamDBit
+//
+//  Created by Andreas Buff on 12/12/15.
+//  Copyright © 2015 Andreas Buff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
